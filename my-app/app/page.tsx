c2b66c78f043bc6c0ce270064f2afa8e43358f55
Larry Le <lle114@student.hbuhsd.edu>
2024-09-05 12:52:27 -0700

    next.js tailwind css build

---
+++ my-app/app/page.tsx
@@ -0,0 +1,5 @@
+import { AppComponentsNavbar } from "@/components/app-components-navbar"
+
+export default function Page() {
+  return <AppComponentsNavbar />
+}
---
+++ my-app/components/app-components-navbar.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import Link from 'next/link'
+
+export function AppComponentsNavbar() {
+  return (
+    <nav className="bg-blue-500 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-white text-2xl font-bold">
+          Travito
+        </Link>
+        <div className="space-x-4">
+          <Link href="/groups" className="text-white hover:text-blue-200">
+            Groups
+          </Link>
+          <Link href="/itinerary" className="text-white hover:text-blue-200">
+            Itinerary
+          </Link>
+          <Link href="/bookings" className="text-white hover:text-blue-200">
+            Bookings
+          </Link>
+          <Link href="/chat" className="text-white hover:text-blue-200">
+            Chat
+          </Link>
+        </div>
+      </div>
+    </nav>
+  )
+}
